b1a62241451de1a6faaea897dbd9d0502077de5a
Nyanjuimarvin <marnyanjui@gmail.com>
2022-02-20 15:35:11 +0300

    Add Handlebars templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,43 +1,17 @@
 import static spark.Spark.*;
+import java.util.*;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
-        get("/",( request, response) ->
-                        "<!DOCTYPE html>" +
-                                "<html>" +
-                                "<head>" +
-                                "<title>Hello Friend!</title>" +
-                                "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                                "</head>" +
-                                "<body>" +
-                                "<h1>Hello From Afar</h1>" +
-                                "<p>Dear Friend,</p>" +
-                                "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
-                                "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-                                "<p>But I like programming a lot, so I've got that going for me. </p>" +
-                                "<p><a href='/images' >P.S. Check out my favorite travel photos here.</a></p>" +
-                                "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-                                "<p>Cheers,</p>" +
-                                "<p>Travel Enthusiast Jane</p>" +
-                                "</body>" +
-                                "</html>"
-                );
+        get("/",( request, response) ->{
+            return new ModelAndView( new HashMap(), "hello.hbs");
+                },new HandlebarsTemplateEngine() );
 
-        get("/images",((request, response) ->
-                       "<!DOCTYPE html>" +
-                        "<html>" +
-                        "<head>" +
-                        "<title>Hello Friend!</title>" +
-                        "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                        "</head>" +
-                        "<body>" +
-                        "<h1>Favorite Traveling Photos</h1>" +
-                        "<ul>" +
-                        "<li><img src='/images/beach.jpg' alt='beach Sunset'/></li>" +
-                        "<li><img src='/images/watchtower.jpg' alt='Watchtower'/></li>" +
-                        "<li><img src='/images/jungle.jpg' alt='Jungle'/></li>" +
-                        "<li><img src='/images/mount.jpg' alt='Mountain'/></li>" +
-                        "</ul>" + "</body>" + "</html>"
-                ));
+        get("/images",(request, response) ->{
+            return new ModelAndView( new HashMap(), "images.hbs");
+        },new HandlebarsTemplateEngine());
     }
 }
